3895e77f337350b9e55f39952b85b8c739484f29
saminoorsyed <saminoorsyed@gmail.com>
2022-07-29 20:26:01 -0400

    start implementing min-heap to prims algo
    figured out heapq, just debugging now

--- graphs/MST.py
+++ graphs/MST.py
@@ -1,4 +1,7 @@
-'''
+
+import heapq
+def Prims(G:list)->list:
+    '''
 input format: a graph is input as an adjacency matrix of the following form:
 input = [ 
   [0, 8, 5, 0, 0, 0, 0], 
@@ -16,28 +19,28 @@ Values to the left of the diagonal represent the
 Output: a list of tuples, wherein each tuple represents an edge of the MST as (v1, v2, 
 weight) 
 '''
-
-def Prims(G:list)->list:
     #store the number of vertices in the graph
     vertices = len(G)
-    # here the index represents the vertex, the number stored represents the node that has been visited 
-    visited = [0]
+    # create a list of tuples from the first row in G that represents the distances from node 0 to each other node
+    min_heap = [[(G[i][j], i, j,) for j in range(len(G[i]))] for i in range (len(G))]
+    #create a heap structure from the list of tuples
+    heapq.heapify(min_heap)
     result = []
     #start a loop to go through all of the nodes until each has been visited
+    visited = [0]
     while len(visited) < vertices:
         #set an arbitrarily high number to begin the comparison at the start of each loop
         minimum = 999999
         # loop through each visited node and check its edges for the least value
-        for i in visited:
-            #check all values in the row relative to 
-            for j in range(len(G[i])):
-                # if j is not 0 (which represents no edge), j is less than the previous minimum and has not been visited before
-                if G[i][j] and G[i][j] < minimum and j not in visited:
-                    minimum = G[i][j]
-                    new_edge = [i, j, G[i][j]]
-        visited.append(new_edge[1])
-        result.append(tuple(new_edge.copy()))
-
+        # to speed this up, we would create a min heap of all the values in this row and then append the deque from first item
+        closest_node = heapq.heappop(min_heap)
+        # 0 is the column value which represents the node traveled to
+        if closest_node[0] not in visited:
+            visited.append(closest_node[2])
+            result.append(closest_node)
+            for i in range(len(G)):
+                newedge = G[closest_node[2]][i]
+                heapq.heappush(min_heap,(newedge,closest_node[2], i))
     return result
 
 if __name__ == '__main__':
