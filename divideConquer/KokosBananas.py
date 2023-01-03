5a9a78f8dc258cf61b42b97b9268b3ae346cf1a4
saminoorsyed <saminoorsyed@gmail.com>
2023-01-02 22:54:10 -0500

    set up for koko's bananas. look at number of piles vs hrs vs number of bananas

---
+++ divideConquer/KokosBananas.py
@@ -0,0 +1,19 @@
+# Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour. Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.Return the minimum integer k such that she can eat all the bananas within h hours.
+
+def minEatingSpeed(piles: list[int], h: int) -> int:
+
+
+
+if __name__ == "__main__":
+    piles = [3,6,7,11]
+    h = 8
+    print(minEatingSpeed(piles, h) )
+# Output: 4
+    piles = [30,11,23,4,20]
+    h = 5
+    print(minEatingSpeed(piles, h) )
+# Output: 30
+    piles = [30,11,23,4,20]
+    h = 6
+    print(minEatingSpeed(piles, h) )
+# Output: 23
