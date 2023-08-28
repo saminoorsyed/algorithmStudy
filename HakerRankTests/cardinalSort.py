5f63df62d7efba50e3292e151c495434f90a8dc6
saminoorsyed <saminoorsyed@gmail.com>
2023-08-28 11:51:32 -0400

    worked on old jp morgan hacker rank

--- HakerRankTests/cardinalSort.py
+++ HakerRankTests/cardinalSort.py
@@ -1,34 +1,16 @@
 # sort a list of numbers in ascending order the number of set digits in their binary notation.
-def cardinal_sort(nums:list[int])->list[int]:
-    """
-    returns a list sorted in ascending order by the second value of each inner list
-    the inner list comes in the form of [originalValue, cardinality]
-    """
-    def countOnes(num):
-        """
-        counts the number of set digits in the binary form of a decimal
-        """
-        count =0
+def cardinal_sort(nums:list[int])-> list[int]:
+    #function to find the cardinality of a number:
+    def cardinality(num: int)-> int:
+        count = 0
         while num:
-            # when the remainder is one, count should be incremented
-            count += num%2
+            count += num % 2
             num = num//2
         return count
-    
-    arr = [[number, countOnes(number)] for number in nums]
-    # use python sorted with a lambda function
-    result =  sorted(arr, key = lambda arr:arr[1] )
-    return result
-
-# set count ones separate so that I can test it
-def countOnes(num):
-        count =0
-        while num:
-            count += num%2
-            num = num//2
-        return count
+    #sort based on cardinality
+    answer = sorted(nums, key = lambda x : cardinality(x))
+    return answer
 if __name__ == "__main__":
     nums = [1,2,3,4,5]
+    # should print [1,2,4,3,5]
     print(cardinal_sort(nums))
-
-    print (countOnes(5))
--- HakerRankTests/maxStep.py
+++ HakerRankTests/maxStep.py
@@ -2,27 +2,22 @@
 # that can be reached given that step lengths can be equal to the interval that the step is taken in, or 
 # it can be 0. for instance on interval 1, the step length can be 0 or 1. On step 2 the interval can be
 # 0 or 2. On interval 3,  the step length can be 3 or 0 and so on.
-def maxIndex(steps, badIndex):
-    maxIndex = 0
-    for step in range(steps+1):
-        maxIndex +=step
-    current_index = maxIndex
-    step = steps
-
-    while True:
-        while current_index > 0 and step > 0:
-            current_index -= step
-            if (current_index == badIndex):
-                current_index+=step
-            step -=1
-        if current_index<=0:
-            return maxIndex
-        else:
-            step = steps
-            current_index = maxIndex-1
-            maxIndex-=1
-            if current_index == badIndex:
-                current_index = maxIndex-1
-                maxIndex-=1
+def max_step(tot_steps: int, no_step:int)-> int:
+    #find the farthest distance without taking into account no_step
+    max_steps = 0
+    for step in range(tot_steps+1):
+        max_steps += step
+    #count backward, avoiding the no step until you get to step 0
+    cur_step = 1
+    while max_steps != 0:
+        cur_step = max_steps
+        for step in range(tot_steps):
+            if cur_step - (tot_steps-step) == no_step:
+                step+=1
+            cur_step -= tot_steps - step
+            if cur_step <= 0:
+                return max_steps
+        max_steps-=1
 if __name__ == "__main__":
-    print(maxIndex(40,10))
+    #should be 819
+    print(max_step(40,10))
